5b2241fa9bd89181faf8aa0f620d94c41347feb0
KingIvan-ai <accurateivan@gmail.com>
2024-12-09 09:22:24 -0500

    Dining room
    The dining room function reads from a text file and describes the dining room and presents the player with options. Depending on the choice, the game may end or the player will end up back in the living room.

--- text-adventure.py
+++ text-adventure.py
@@ -64,6 +64,66 @@ Will you steal the key from the monster this time? (Enter 'yes' or 'no')")
         else: 
             print("Invalid choice. Please enter yes or no.")
 
+def diningRoom(world):
+    if len(locations) > 6:
+        print("While you were going back and forth between the living room and the dining room\n\
+a ghost attacks you from behind and kills you")
+        return print("GAME OVER")
+    if {"key", "axe"}.issubset(world["inv"]):
+        world["loc"] = "living room 2"
+        livingRoom2(world)
+        return
+    
+    while True:
+        if locations.count("dining room") < 2:
+                with open("rooms.txt", "r") as file:
+                        fileContents = file.read()
+                        fileParagraphs = fileContents.split("\n\n")
+                        print(fileParagraphs[3])
+        else:
+            print("You have returned to the dining room. This time, you notice there is another door.\n\
+you may enter through the other door or return to the living room. Which will you choose?\n\
+(Enter 'living room' or 'new door')")
+            userInput = input(">")
+            if userInput == "living room":
+                    world["loc"] = "living room"
+                    livingRoom(world)
+            if userInput == "new door":
+                    print("Behind the door, there is a monster that jumps out and kills you.")
+                    return print("GAME OVER")
+
+        vaseChoice = input("> ")
+
+        if(vaseChoice == "yes" and locations.count("living room") == 0):
+            with open("rooms.txt", "r") as file:
+                fileContents = file.read()
+                fileParagraphs = fileContents.split("\n\n")
+                print(fileParagraphs[4])
+            world["inv"].append("axe")
+            print(world["inv"])
+            world["loc"] = "living room"
+            locations.append("living room")
+            print(locations)
+            livingRoom(world)
+            break
+        elif(vaseChoice == "yes" and locations.count("living room") > 0):
+            print("You grab the axe and return to the living room.")
+            world["inv"].append("axe")
+            print(world["inv"])
+            world["loc"] = "living room"
+            locations.append("living room")
+            print(locations)
+            livingRoom(world)
+            break
+        elif(vaseChoice == "no"):
+            with open("rooms.txt", "r") as file:
+                fileContents = file.read()
+                fileParagraphs = fileContents.split("\n\n")
+                print(fileParagraphs[5])
+                break
+        else: 
+            print("Invalid choice. Please enter yes or no.")
+
 def main():
     world = {}
     world["loc"] = "walkway"
