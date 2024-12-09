4c8381ea054a56f17bbf870c68b7cdd5b652ac1e
KingIvan-ai <accurateivan@gmail.com>
2024-12-09 09:17:56 -0500

    The walkway function
    The walkway function reads from a file describing the walkway and then provides the character with the opportunity to choose to enter either the living room or the dining room.

--- text-adventure.py
+++ text-adventure.py
@@ -1,3 +1,27 @@
+locations = []
+
+def walkway(world):
+    with open("rooms.txt", "r") as file:
+        fileContents = file.read()
+        fileParagraphs = fileContents.split("\n\n")
+        print(fileParagraphs[1])
+    while True:
+        userInput = input().lower()
+        if userInput == "living room":
+            locations.append(userInput)
+            print(locations)
+            world["loc"] = "living room"
+            livingRoom(world)
+            break
+        elif userInput == "dining room":
+            locations.append(userInput)
+            print(locations)
+            world["loc"] = "dining room"
+            diningRoom(world)
+            break
+        else:
+            print("Invalid input! Please enter 'living room' or 'dining room'.")
+
 def main():
     world = {}
     world["loc"] = "walkway"
