7ae120bd0fa525e6d7414fd089be65584fdcdcf3
KingIvan-ai <accurateivan@gmail.com>
2024-12-09 09:20:28 -0500

    The living room function
    The living room function reads from a text file to describe the living room and the options the player has upon entering. It also calls the dining room and living room 2 functions depending on what choice the player makes.

--- text-adventure.py
+++ text-adventure.py
@@ -22,6 +22,48 @@ def walkway(world):
         else:
             print("Invalid input! Please enter 'living room' or 'dining room'.")
 
+def livingRoom(world):
+    if len(locations) > 6:
+        print("While you were going back and forth between the living room and the dining room\n\
+a ghost attacks you from behind and kills you")
+        return print("GAME OVER")
+    if {"key", "axe"}.issubset(world["inv"]):
+        world["loc"] = "living room 2"
+        livingRoom2(world)
+        return
+
+    while True:
+        if locations.count("living room") < 2:
+            with open("rooms.txt", "r") as file:
+                        fileContents = file.read()
+                        fileParagraphs = fileContents.split("\n\n")
+                        print(fileParagraphs[2])
+        else:
+            print("You have returned to the living room with the monster and your friend.\n\
+Will you steal the key from the monster this time? (Enter 'yes' or 'no')")
+
+        monsterChoice = input("> ")
+
+        if(monsterChoice == "yes"):
+            print("Excellent. You have successfully stolen the key!\n"
+                "Now, it's time to go back to the dining room.")
+            world["inv"].append("key")
+            print(world["inv"])
+            world["loc"] = "dining room"
+            locations.append("dining room")
+            print(locations)
+            diningRoom(world)
+            break
+        elif(monsterChoice == "no"):
+            print("You decide to not steal the dog's jewelry and instead go to the dining room.")
+            world["loc"] = "dining room"
+            locations.append("dining room")
+            print(locations)
+            diningRoom(world)
+            break
+        else: 
+            print("Invalid choice. Please enter yes or no.")
+
 def main():
     world = {}
     world["loc"] = "walkway"
