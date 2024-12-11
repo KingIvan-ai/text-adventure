2a2fa14cfcb065473fcdfd03061b0c3efd6c7cb8
KingIvan-ai <accurateivan@gmail.com>
2024-12-11 10:35:19 -0500

    Text updates and input validation
    I updated the text to fit the game better and validated the input in for turn function

--- fight.py
+++ fight.py
@@ -13,28 +13,34 @@ def diceRoll(roll):
     return diceList
 
 def turn(attacker, defender, who):
-    if who == "player":
-        print("Pick an option: ")
-        print("1: Attack")
-        print("2: Heal")
-        decision = input() # Ideally, we would validate their input
-    else:
-        if (randint(1,2) == 1):  # 50% chance
-            decision = "1" # enemy decided to attack
+    while True:
+        if who == "player":
+            print("Pick an option (enter '1' or '2'): ")
+            print("1: Attack")
+            print("2: Heal")
+            decision = input() # Ideally, we would validate their input
         else:
-            decision = "2" # enemy decided to do nothing
+            if (randint(1,2) == 1):  # 50% chance
+                decision = "1" # enemy decided to attack
+            else:
+                decision = "2" # enemy decided to do nothing
     
-    # Apply effect of decision
-    if decision == "1":
-        defender["hp"] -= attacker["atk"]
-        print(f"{attacker['name']} hits {defender['name']} for {attacker['atk']} damage")
-    if decision == "2":
-        healAmt = sum(diceRoll("1d4"))
-        attacker["hp"] += healAmt
-        print(f"{attacker['name']} rests and recovers {healAmt} hp")
+        # Apply effect of decision
+        if decision == "1":
+            defender["hp"] -= attacker["atk"]
+            print(f"{attacker['name']} hits {defender['name']} for {attacker['atk']} damage")
+            break
+        elif decision == "2":
+            healAmt = sum(diceRoll("1d4"))
+            attacker["hp"] += healAmt
+            print(f"{attacker['name']} rests and recovers {healAmt} hp")
+            break
+        else:
+            print("Invalid input. Please enter '1' or '2'")
 
 def battle(player, enemy):
-    print(f"A {enemy['name']} approches {player['name']}, ready to fight")
+    print(f"You have returned to the living room.\nThis time, the monster is awake and ready to fight!\n"
+          "The Monster comes towards you and the fight begins!")
 
     while True:
         turn(player, enemy, "player")
